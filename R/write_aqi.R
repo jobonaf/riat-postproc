03e2370e7e29176ebcd6dc1e230c18403a3d6a65
Giovanni Bonafè <midable@gmail.com>
2024-12-04 14:22:18 +0100

    write_aqi.R writes also base case

--- R/write_aqi.R
+++ R/write_aqi.R
@@ -1,4 +1,4 @@
-library("glue")
+library(glue)
 library(readr)
 library(dplyr)
 
@@ -8,13 +8,17 @@ write_aqi <- function(
     filedelta = glue("data/data_prepair_{model}/run/{run}/post_proc_output_dir/maps_aqi/{poll}TP1_point2_del.csv"),
     fileout = glue("{run}_{model}_{poll}.csv")) {
   bdat <- read.csv(filebase)
+  colnames(bdat) <- c("x","y","aqi_base")
   ddat <- read.csv(filedelta)
-  dat <- bind_cols(bdat,ddat)
-  dat$value <- dat[,3]+dat[,6]
-  dat$value[dat$value < 0] <- NA
-  dat %>% 
-    transmute(x=xutm.km., y=yutm.km., value=round(value,2)) %>%
-    filter(!is.na(value)) %>%
+  colnames(ddat) <- c("x","y","aqi_delta")
+  dat <- bind_cols(
+    bdat,
+    ddat %>% select(aqi_delta)
+  ) %>%
+    mutate(aqi_scenario=ifelse(aqi_base<0,NA,round(aqi_base+aqi_delta,2)),
+           aqi_base=round(aqi_base,2),
+           aqi_delta=round(aqi_delta,2)) %>%
+    filter(!is.na(aqi_scenario)) %>%
     write_csv(., file=fileout)
 }
 
@@ -26,3 +30,11 @@ write_aqi(model="ninfa_er", poll="PM10", run="pniec2030b")
 write_aqi(model="ninfa_er", poll="NO2",  run="pniec2030b")
 write_aqi(model="farm_pi",  poll="PM10", run="pniec2030b")
 write_aqi(model="farm_pi",  poll="NO2",  run="pniec2030b")
+
+write_aqi(model="farm_pi",  poll="PM25", run="pniec2030a")
+write_aqi(model="farm_pi",  poll="PM25", run="pniec2030b")
+
+write_aqi(model="ninfa_er", poll="NO2",  run="pniec2030e")
+write_aqi(model="ninfa_er", poll="PM10",  run="pniec2030e")
+write_aqi(model="ninfa_er", poll="PM25",  run="pniec2030e")
+

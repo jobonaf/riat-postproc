0849d0dfe57f14defec4712fe9706a01f80b1b6b
Giovanni Bonafè <midable@gmail.com>
2025-01-14 12:20:51 +0100

    EEA data: added data for cities (LAU)

--- R/prepare_eea_data.R
+++ R/prepare_eea_data.R
@@ -1,9 +1,14 @@
-# data extracted with following query
-# https://discomap.eea.europa.eu/App/AQViewer/index.html?fqn=Airquality_Dissem.ebd.countries_and_nuts&UrbanisationDegree=All Areas (incl.unclassified)&CountryOrTerritory=Italy&Year=2013,2017&DataAggregation=P1Y
+# data extracted with following queries
+# DataExtract_NUTS:   https://discomap.eea.europa.eu/App/AQViewer/index.html?fqn=Airquality_Dissem.ebd.countries_and_nuts&UrbanisationDegree=All Areas (incl.unclassified)&CountryOrTerritory=Italy&Year=2013,2017&DataAggregation=P1Y
+# DataExtract_cities: https://discomap.eea.europa.eu/App/AQViewer/index.html?fqn=Airquality_Dissem.ebd.cities&ScenarioDescription=Baseline%20from%20WHO%202021%20AQG&Year=2022&Sex=Total#
+
+# packages
 library(readr)
 library(tidyr)
 library(glue)
-dat <- read_csv("out/eea/DataExtract.csv")
+
+# NUTS
+dat <- read_csv("out/eea/DataExtract_NUTS.csv")
 dat %>%
   distinct(
     `NUTS Code`, 
@@ -21,4 +26,22 @@ for (pp in c("PM2.5","NO2")) {
   dd %>% 
     filter(nchar(`NUTS Code`)==4, `Air Pollutant`==pp) %>% 
     write_csv(., glue("out/eea/{pp}_REGIONE_dati_EEA.csv"))
+}
+
+# cities
+dat <- read_csv("out/eea/DataExtract_cities.csv")
+dat %>%
+  distinct(
+    `City Code`, 
+    `City Or Territory`, 
+    Year, 
+    `Air Pollutant`, 
+    `Air Pollution Average [ug/m3]`) %>%
+  pivot_wider(
+    names_from = Year, 
+    values_from = `Air Pollution Average [ug/m3]`) -> dd
+for (pp in c("PM2.5","NO2")) {
+  dd %>% 
+    filter(`Air Pollutant`==pp) %>% 
+    write_csv(., glue("out/eea/{pp}_CITTA_dati_EEA.csv"))
 }
